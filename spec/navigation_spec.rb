5cd658dbbcc30be137c2d943582e760907506224
Linda Le <yueling1222@gmail.com>
2019-12-03 11:43:30 -0700

    Add parenthesis around number for favorites link in nav

--- spec/navigation_spec.rb
+++ spec/navigation_spec.rb
@@ -21,7 +21,7 @@ RSpec.describe 'Navigation bar', type: :feature do
     it 'can see a favorites link' do
       visit '/'
       within 'nav' do
-      expect(page).to have_link("My Favorites #{@dog_1.count_favorites}")
+      expect(page).to have_link("My Favorites (#{@dog_1.count_favorites})")
       end
     end
   end
