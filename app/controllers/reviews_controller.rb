9023663781cebc8578fadaa179932adce6deb754
Becky Robran <rerobran@gmail.com>
2019-12-03 16:16:53 -0700

    Adds flash messages to create action

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,5 +4,21 @@ class ReviewsController < ApplicationController
   end
 
   def create
+    shelter = Shelter.find(params[:id])
+
+    @review = shelter.reviews.new(review_params)
+      if @review.save
+      flash[:notice] = "You have created a review!"
+      redirect_to "/shelters/#{shelter.id}"
+    else
+      flash[:alert] = "You have not filled in all the necessary fields to create a review."
+      redirect_to "/shelters/#{shelter.id}/reviews/new"
+    end
+  end
+
+  private
+
+  def review_params
+    params.permit(:title, :rating, :content, :image_url)
   end
 end
