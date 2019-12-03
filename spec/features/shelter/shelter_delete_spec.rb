650ab7fc6dbd34d7f00a015b9adb10931493ef61
Becky Robran <rerobran@gmail.com>
2019-12-02 21:34:06 -0700

    adds pets and reviews to shelters delete testing

--- spec/features/shelter/shelter_delete_spec.rb
+++ spec/features/shelter/shelter_delete_spec.rb
@@ -9,6 +9,16 @@ RSpec.describe 'As a visitor', type: :feature do
                                      city: 'Denver',
                                      state: 'CO',
                                      zip_code: '80004')
+      @dog_1 = @shelter_1.pets.create!(image_url: '/',
+                        name: 'Tofu',
+                        approximate_age: 4,
+                        sex: 'M',
+                        description: 'I am a neutered male, white Terrier Mix who loves to play fetch.')
+      # @review_1 = Review.create!(title: 'Denver Pet Adoption Process',
+      #                             rating: 4,
+      #                             content: 'Denver Pets Shelter makes the process of adopting a pet easy and stress free.',
+      #                             image_url: 'https://media.wired.com/photos/5dd593a829b9c40008b179b3/master/w_2560%2Cc_limit/Cul-BabyYoda_mandalorian-thechild-1_af408bfd.jpg',
+      #                             shelter: @shelter_1)
 
     end
 
