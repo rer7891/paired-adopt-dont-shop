81b3b08becd60445c806b661b20b39c733ef6e19
Becky Robran <rerobran@gmail.com>
2019-12-02 21:33:38 -0700

    Adds destroy_all to all resources and adds review seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+Pet.destroy_all
+Review.destroy_all
+Shelter.destroy_all
 shelter_1 = Shelter.create!(name: 'Denver Pet Shelter',
                            address: '123 Colfax Ave',
                            city: 'Denver',
@@ -36,3 +38,13 @@ shelter_2.pets.create!(image_url: 'https://img1.looper.com/img/gallery/the-untol
                         approximate_age: 1,
                         sex: 'F',
                         )
+review_1 = Review.create!(title: 'Denver Pet Adoption Process',
+                           rating: 4,
+                           content: 'Denver Pet Shelter makes the process
+                                    of adopting a pet easy and stress free.',
+                           image_url: 'https://media.wired.com/photos/5dd593a829b9c40008b179b3/master/w_2560%2Cc_limit/Cul-BabyYoda_mandalorian-thechild-1_af408bfd.jpg',
+                           shelter: shelter_1)
+review_2 = Review.create!(title: 'Denver Pet Is the Best',
+                            rating: 5,
+                            content: 'Denver Pet Shelter helped us find a new family member. I can never thank them enough.',
+                            shelter: shelter_1)
