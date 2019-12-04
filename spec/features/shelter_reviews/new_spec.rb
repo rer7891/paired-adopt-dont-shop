ae032f250be74e7bc9f542467a5ed0701b60cb44
Becky Robran <rerobran@gmail.com>
2019-12-03 18:37:49 -0700

    Adds testing for flash messages

--- spec/features/shelter_reviews/new_spec.rb
+++ spec/features/shelter_reviews/new_spec.rb
@@ -4,16 +4,16 @@ RSpec.describe "As a visitor", type: :feature do
   describe "I can go to a shelter's show page" do
     describe "and see a link to create a new review" do
       it "which takes me to a form where I can create a new review" do
-        @shelter_1 = Shelter.create!(name: 'Denver Pet Shelter',
+        shelter_1 = Shelter.create!(name: 'Denver Pet Shelter',
                                    address: '123 Colfax Ave',
                                    city: 'Denver',
                                    state: 'CO',
                                    zip_code: '80004')
-        visit "/shelters/#{@shelter_1.id}"
+        visit "/shelters/#{shelter_1.id}"
 
         click_link "Create A New Review"
 
-        expect(current_path).to eql("/shelters/#{@shelter_1.id}/reviews/new")
+        expect(current_path).to eql("/shelters/#{shelter_1.id}/reviews/new")
 
         fill_in "Title",     with: "Best New Shelter"
         select('4', :from => 'Rating')
@@ -21,13 +21,36 @@ RSpec.describe "As a visitor", type: :feature do
         fill_in "image_url", with: "https://picsum.photos/id/200/1920/1280"
         click_on "Create A New Review"
 
-        expect(current_path).to eql("/shelters/#{@shelter_1.id}")
+        expect(current_path).to eql("/shelters/#{shelter_1.id}")
+
+        expect(page).to have_content("You have created a review!")
         review = Review.last
         expect(review.title).to eql("Best New Shelter")
         expect(page).to have_content(review.title)
         expect(page).to have_content(review.rating)
         expect(page).to have_content(review.content)
-      end 
+      end
+
+      it "will redirect to shelters/:id/reviews/new if a form is not fill out completely" do
+        shelter_1 = Shelter.create!(name: 'Denver Pet Shelter',
+                                   address: '123 Colfax Ave',
+                                   city: 'Denver',
+                                   state: 'CO',
+                                   zip_code: '80004')
+        visit "/shelters/#{shelter_1.id}"
+
+        click_link "Create A New Review"
+
+        expect(current_path).to eql("/shelters/#{shelter_1.id}/reviews/new")
+
+        #There is no title or rating filled in.
+        fill_in "Content",   with: "A brand new facility that is first rate. Their staff is friendly and helpful."
+        fill_in "image_url", with: "https://picsum.photos/id/200/1920/1280"
+        click_on "Create A New Review"
+
+        expect(current_path).to eql("/shelters/#{shelter_1.id}/reviews/new")
+        expect(page).to have_content( "You have not filled in all the necessary fields to create a review.")
+      end
     end
   end
 end
