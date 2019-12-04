31ca288272970e9b846db4164a28396fbc73943f
Becky Robran <rerobran@gmail.com>
2019-12-04 15:40:07 -0700

    Adds before action with favorites cookie method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :favorite
+
+  def favorite
+    cookies[:favorites] ||= " "
+  end
 end
