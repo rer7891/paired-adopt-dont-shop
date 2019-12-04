b3b2a1982589c54b42285629a2a1b7689ad2b538
Becky Robran <rerobran@gmail.com>
2019-12-03 18:36:52 -0700

    Adds flash messages to creating a review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,15 +5,15 @@ class ReviewsController < ApplicationController
 
   def create
     shelter = Shelter.find(params[:id])
-
-    @review = shelter.reviews.new(review_params)
-      if @review.save
-      flash[:notice] = "You have created a review!"
-      redirect_to "/shelters/#{shelter.id}"
-    else
+    #flash messages for users once a review is created.
+    review = shelter.reviews.new(review_params)
+      if review.save
+        flash[:notice] = "You have created a review!"
+        redirect_to "/shelters/#{shelter.id}"
+     else
       flash[:alert] = "You have not filled in all the necessary fields to create a review."
       redirect_to "/shelters/#{shelter.id}/reviews/new"
-    end
+     end
   end
 
   private
