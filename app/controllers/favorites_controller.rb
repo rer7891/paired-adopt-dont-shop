a67c1595bb99eddd7e6f64ffa8e88b3f1f6e05a9
Becky Robran <rerobran@gmail.com>
2019-12-04 20:30:41 -0700

    Updates favorites controller update method

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,17 +1,17 @@
 class FavoritesController < ApplicationController
   def index
-    ids = cookies[:favorites].split(",").uniq
-    @favorite_pets = Pet.find(ids)
-    @favorite_count = @favorite_pets.length
   end
 
   def update
-   pet_id = params[:id]
-   cookies[:favorites] += "#{pet_id},"
+  pet_id = params[:id]
+
+  if session[:favorite].key?(pet_id)
+    flash[:error] = "You have already favorited this pet!"
+  else
+    @favorites.add_favorite(pet_id)
+    session[:favorite] = @favorites.content
     flash[:success] = "You added a new pet to your favorites!"
-   # else
-   #   flash[:error] = "You have already favorited this pet!"
-   # end
+  end
 
    redirect_to "/pets/#{pet_id}"
   end
