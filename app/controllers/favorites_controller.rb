5bbb471b1e2f7e003d9913a98843ce28025829b6
Becky Robran <rerobran@gmail.com>
2019-12-05 13:14:02 -0700

    Adds some filter options for favorites show page

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,18 +1,21 @@
 class FavoritesController < ApplicationController
   def index
+    @pets = Pet.all
+    #keys = @favorites.keys.to_i
+    #@pets = Pet.where(id: [keys])
   end
 
   def update
-  pet_id = params[:id]
 
-  if @favorites.content.key?(pet_id)
+  pet = Pet.find(params[:id])
+  if @favorites.content.keys.include?(pet.id.to_s)
     flash[:error] = "You have already favorited this pet!"
   else
-    @favorites.add_favorite(pet_id)
+    @favorites.add_favorite(pet.id)
     session[:favorite] = @favorites.content
     flash[:success] = "You added a new pet to your favorites!"
   end
-   redirect_to "/pets/#{pet_id}"
+   redirect_to "/pets/#{pet.id}"
   end
 
 end
