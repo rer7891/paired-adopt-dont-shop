1b41c25f0348631b2cca3e36ee20b69279639ed6
Becky Robran <rerobran@gmail.com>
2019-12-05 11:17:40 -0700

    Updates update action conditional statement

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -5,14 +5,14 @@ class FavoritesController < ApplicationController
   def update
   pet_id = params[:id]
 
-  if session[:favorite].key?(pet_id)
+  if @favorites.content.key?(pet_id)
     flash[:error] = "You have already favorited this pet!"
   else
     @favorites.add_favorite(pet_id)
     session[:favorite] = @favorites.content
     flash[:success] = "You added a new pet to your favorites!"
   end
-
    redirect_to "/pets/#{pet_id}"
   end
+
 end
