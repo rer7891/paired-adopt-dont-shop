01e22a29d5d7690d180fb3db0fe722aad2968c46
Linda Le <yueling1222@gmail.com>
2019-12-09 22:39:31 -0700

    Revise relationships in pet spec file

--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -21,6 +21,7 @@ describe Pet, type: :model do
 
   describe 'relationships' do
     it {should belong_to :shelter}
+    it {should have_many :pet_applications}
+    it {should have_many(:applications).through(:pet_applications)}
   end
-
-end
+end 
