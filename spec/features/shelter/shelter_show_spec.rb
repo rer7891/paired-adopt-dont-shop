ade9a50789de771dfaffea696937c90d1c3d3be8
Becky Robran <rerobran@gmail.com>
2019-12-10 20:33:03 -0700

    Adds shelter review ave and application count to show page and feature test

--- spec/features/shelter/shelter_show_spec.rb
+++ spec/features/shelter/shelter_show_spec.rb
@@ -48,7 +48,45 @@ RSpec.describe 'Shelter show page', type: :feature do
 
       click_on('Adoptable Pets')
       expect(current_path).to eq "/shelters/#{@shelter_1.id}/pets"
+    end
 
+    it 'can see a shelters review rating ave' do
+      review_1 = @shelter_1.reviews.create!(title: 'Denver Pet Adoption Process',
+                                 rating: 2,
+                                 content: 'Denver Pets Shelter makes the process of adopting a pet easy and stress free.',
+                                 image_url: "https://media.wired.com/photos/5dd593a829b9c40008b179b3/master/w_2560%2Cc_limit/Cul-BabyYoda_mandalorian-thechild-1_af408bfd.jpg")
+      review_2 = @shelter_1.reviews.create!(title: 'Denver Pet Is the Best',
+                                rating: 5,
+                                content: 'Denver Pet Shelter helped us find a new family member. I can never thank them enough.')
+
+      visit "/shelters/#{@shelter_1.id}"
+      expect(page).to have_content("#{@shelter_1.name} has an average review rating of #{@shelter_1.review_ave}%")
+    end
+
+    it 'can see the number of applications on file for that shelter' do
+      application_1 = @dog_1.applications.create!(name: "Becky Robran",
+                                                 address: "12342 Main Street",
+                                                 city: "Broomfield",
+                                                 state: "CO",
+                                                 zip: "34533",
+                                                 phone_number: "43253424324",
+                                                 description: "I will be a good dog parent.")
+     dog_2 = @shelter_1.pets.create!(image_url: '/',
+                        name: 'Kuma',
+                        description: 'I am an energetic, black Shiba Inu.',
+                        approximate_age: 1,
+                        sex: 'F',
+                        favorite_status: false)
+
+      application_2 = dog_2.applications.create!(name: "Linda Le",
+                                                 address: "12342 Main Street",
+                                                 city: "Broomfield",
+                                                 state: "CO",
+                                                 zip: "34533",
+                                                 phone_number: "43253424324",
+                                                 description: "I will be a good dog parent.")
+    visit "/shelters/#{@shelter_1.id}"
+    expect(page).to have_content("#{@shelter_1.name} has #{@shelter_1.application_count} applications on file.")
     end
   end
 end
